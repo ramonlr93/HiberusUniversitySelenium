afb71ad8ae857ebce7e92f93457145d153a4354c
Irisrm997 <irisrm997@gmail.com>
2022-04-18 21:10:28 +0200

    first commit

---
+++ src/main/java/com/hiberus/university/iris/maven/first/App.java
@@ -0,0 +1,13 @@
+package com.hiberus.university.iris.maven.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/hiberus/university/iris/maven/first/AppTest.java
@@ -0,0 +1,20 @@
+package com.hiberus.university.iris.maven.first;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
