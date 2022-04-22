f5927d2a381f26bc370b49c1a360f9403740a3eb
rgarciam <rgarciam@hiberus.com>
2022-04-22 13:10:21 +0200

    [demo01] Dependencias del proyecto

--- src/main/java/com/hiberus/university/selenium/App.java
+++ src/main/java/com/hiberus/university/selenium/App.java
@@ -4,10 +4,8 @@ package com.hiberus.university.selenium;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World! QA" );
     }
 }
