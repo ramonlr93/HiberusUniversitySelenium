8e3be85cb6adecb1acfc7c02ea6062bb3c0aaf6c
javieruriz1989gmail.com <zexiBLOODLUST>
2022-04-26 17:19:43 +0200

    ejemplo

--- src/main/java/com/hiberus/university/selenium/App.java
+++ src/main/java/com/hiberus/university/selenium/App.java
@@ -5,7 +5,7 @@ package com.hiberus.university.selenium;
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World! QA" );
+    public static void main ( String[] args ) {
+        System.out.println( "Hello World! QA FORMACION" );
     }
 }
