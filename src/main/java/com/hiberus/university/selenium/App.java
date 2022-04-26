f2c221f68d3ba079f8d70cb3fde1a4bd64bec962
Neku94 <nekusakuraba94@hotmail.com>
2022-04-26 17:32:52 +0200

    Modificación

--- src/main/java/com/hiberus/university/selenium/App.java
+++ src/main/java/com/hiberus/university/selenium/App.java
@@ -7,6 +7,9 @@ package com.hiberus.university.selenium;
 public class App {
     public static void main( String[] args ) {
         System.out.println("Hello World! QA");
+        for (int i=0; i<5; i++){
+            System.out.println("Vuelta: " + i);
+        }
         System.out.println("Modificación");
     }
 }
