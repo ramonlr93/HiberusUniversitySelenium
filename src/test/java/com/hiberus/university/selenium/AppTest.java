b8c939de4839e749f9e904bc6271321443b415c1
I-Marin <arielmarinfalcon@gmail.com>
2022-04-27 20:19:57 +0200

    Commit con Ejercicio 1 y 2

--- src/test/java/com/hiberus/university/selenium/AppTest.java
+++ src/test/java/com/hiberus/university/selenium/AppTest.java
@@ -2,19 +2,38 @@ package com.hiberus.university.selenium;
 
 import static org.junit.Assert.assertTrue;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+        String userProfile = "C:\\Users\\pue\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\";
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("user-data-dir=" + userProfile);
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        driver.get("https://www.hiberus.com/");
+
+
+        System.out.println(driver.getPageSource());
+
+        driver.close();
+
     }
 }
