d2346aaad15930d645395c899cb0a82a63a55ebb
marioEzquerro <marioezquerroruiz@gmail.com>
2022-04-27 19:33:03 +0200

    Ejercicio 2

--- src/main/java/com/hiberus/university/selenium/Comandos.java
+++ src/main/java/com/hiberus/university/selenium/Comandos.java
@@ -1,18 +1,20 @@
 package com.hiberus.university.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.util.concurrent.TimeUnit;
 
-public class App {
+public class Comandos {
     public static WebDriver driver;
 
     public static void main( String[] args ) {
         // Paso 1
         String userProfile = "C:\\Users\\pue\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\";
-        final String ruta = "https://saucedemo.com/";
+        String ruta = "https://hiberus.com/";
         WebDriverManager.chromedriver().setup();  //Cargar ChroneDriver
         ChromeOptions options = new ChromeOptions();  //Crear instancia para opciones de chrome
         options.addArguments("user-data-dir=" + userProfile);
@@ -21,21 +23,9 @@ public class App {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        //Paso 2
         driver.get(ruta);
-
-        //Paso 3 y 4
-        String title = driver.getTitle();
-        //int length_title = driver.getTitle().length();
-        System.out.println(title + ' ' + title.length());
-
-        //Paso 5
-        String url = driver.getCurrentUrl();
-        System.out.println("La url es " + url);
-        if (url.equals(ruta))
-            System.out.println("La url es " + url);
-
-
+        driver.findElement(By.xpath("//a[@href='/consultoria-y-estrategia-de-negocio']")).click();
+        driver.navigate().back();
 
 
     }
