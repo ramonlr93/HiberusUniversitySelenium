93f804dd3ab2f5df871a0e41c552d0c1bde600ae
angellafuentesinues <lafuente.angel@gmail.com>
2022-04-27 12:45:46 +0200

    configuracion inicial

--- src/main/java/com/hiberus/university/selenium/App.java
+++ src/main/java/com/hiberus/university/selenium/App.java
@@ -1,11 +1,36 @@
 package com.hiberus.university.selenium;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World! QA FORMACION" );
+    public static WebDriver driver;
+
+    public static void main( String[] args ) throws InterruptedException {
+
+        String userProfile= "C:\\Users\\usuario\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\";
+        WebDriverManager.chromedriver().setup(); // cargar Chromedriver
+        ChromeOptions options = new ChromeOptions(); // Crear instancia para opciones de chrome
+        options.addArguments("user-data-dir=" + userProfile); //
+
+        driver= new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        // Paso 2
+        driver.get("https://www.hiberus.com/");
+
+        driver.quit();
+
+
+        System.out.println( "\n" + "\n" + "Este es el valor que devuelve: " );
     }
 }
