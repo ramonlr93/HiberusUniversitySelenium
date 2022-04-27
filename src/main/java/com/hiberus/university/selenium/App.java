fe5ce000a251488f599da0e2ace31d4e7b5f82f4
Isabel Ezquerra Soriano <iezquerra@hiberus.com>
2022-04-27 10:29:16 +0200

    cargainicial

--- src/main/java/com/hiberus/university/selenium/App.java
+++ src/main/java/com/hiberus/university/selenium/App.java
@@ -1,11 +1,33 @@
 package com.hiberus.university.selenium;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  *
  */
 public class App {
+
+    public static WebDriver driver;
     public static void main( String[] args ) {
-        System.out.println( "Hello World! QA FORMACION" );
+        String userProfile= "C:\\Users\\iezquerra\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\";
+        WebDriverManager.chromedriver().setup(); // cargar Chromedriver
+        ChromeOptions options = new ChromeOptions(); // Crear instancia para opciones de chrome
+        options.addArguments("user-data-dir=" + userProfile);
+        driver= new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        // Paso 2
+        driver.get("https://www.hiberus.com/");
+        driver.close();
+
+
+        System.out.println( "\n" + "\n" + "Este es el valor que devuelve: " );
     }
 }
--- src/test/java/com/hiberus/university/selenium/AppTest.java
+++ src/test/java/com/hiberus/university/selenium/AppTest.java
@@ -2,19 +2,41 @@ package com.hiberus.university.selenium;
 
 import static org.junit.Assert.assertTrue;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+
+    public static WebDriver driver;
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
+        String userProfile= "C:\\Users\\iezquerra\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\";
+        WebDriverManager.chromedriver().setup(); // cargar Chromedriver
+        ChromeOptions options = new ChromeOptions(); // Crear instancia para opciones de chrome
+        options.addArguments("user-data-dir=" + userProfile);
+        driver= new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        // Paso 2
+        driver.get("https://www.hiberus.com/");
+        driver.close();
+
+
+        System.out.println( "\n" + "\n" + "Este es el valor que devuelve: " );
+
         assertTrue( true );
     }
 }
