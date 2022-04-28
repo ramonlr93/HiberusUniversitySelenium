90b3c9af1401153e4417c098630cc3a4202d1deb
javieruriz1989gmail.com <zexiBLOODLUST>
2022-04-28 09:48:44 +0200

    Cancelar commit

--- src/main/java/com/hiberus/university/selenium/App.java
+++ src/main/java/com/hiberus/university/selenium/App.java
@@ -1,47 +1,11 @@
 package com.hiberus.university.selenium;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.concurrent.TimeUnit;
-
 /**
  * Hello world!
  *
  */
 public class App {
-   public static WebDriver driver;
-    public static void main( String[] args ) {
-        String userProfile="C:\\Users\\pue\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\";
-        WebDriverManager.chromedriver().setup(); //Cargar ChromeDriver
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("user-data-dir="+userProfile);
-
-        driver=new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-
-        // Paso 2
-        driver.get("https://www.hiberus.com/");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        //paso3
-        driver.findElement(By.xpath("//a[@href='/consultoria-y-estrategia-de-negocio']")).click();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        driver.navigate().back();
-
-        //System.out.println(url);
+    public static void main ( String[] args ) {
+        System.out.println( "Hello World! QA FORMACION" );
     }
 }
