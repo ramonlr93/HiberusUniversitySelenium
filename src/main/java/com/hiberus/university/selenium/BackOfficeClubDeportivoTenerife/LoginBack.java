fd162ce93f5f89e419723dbf148d03fadea28cf7
iezquerra <isabel.ezquerra83@gmail.com>
2022-05-02 12:45:02 +0200

    Login Cita Previa

---
+++ src/main/java/com/hiberus/university/selenium/BackOfficeClubDeportivoTenerife/LoginBack.java
@@ -0,0 +1,41 @@
+package com.hiberus.university.selenium.BackOfficeClubDeportivoTenerife;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.WebDriver.*;
+
+public class LoginBack {
+    public static WebDriver driver;
+
+    public static void main(String[] args) {
+        String userProfile = "C:\\Users\\iezquerra\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\";
+        WebDriverManager.chromedriver().setup(); // cargar Chromedriver
+        ChromeOptions options = new ChromeOptions(); // Crear instancia para opciones de chrome
+        options.addArguments("user-data-dir=" + userProfile);
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        //Paso 2
+        driver.get("https://citaprevia.demohiberus.com/auth");
+
+        //Paso 3
+        WebElement element = driver.findElement(By.id("mat-input-0"));
+        element.sendKeys("iezquerra");
+
+        //Paso 4
+        driver.findElement(By.id("mat-input-1")).sendKeys("27Abril2022!!");
+
+        //Paso 5
+       driver.findElement(By.xpath("//span[contains(@class,'mat-button-focus-overlay')]")).click();
+
+        driver.quit();
+    }
+}
