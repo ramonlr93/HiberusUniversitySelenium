df6745b51354cabc260f59237984534e65be15e7
LauraDelRioMartin <laura_del_rio@hotmail.com>
2022-05-02 12:39:50 +0200

    ValidarLoginIncorrecto modificado

--- src/main/java/com/hiberus/university/selenium/login/ValidarLoginIncorrecto.java
+++ src/main/java/com/hiberus/university/selenium/login/ValidarLoginIncorrecto.java
@@ -25,22 +25,22 @@ public class ValidarLoginIncorrecto {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        //Paso 1
+        // Paso 1
         driver.get("https://www.saucedemo.com");
 
-        //Paso 2
+        // Paso 2
         // Escribir el username
         WebElement userBox = driver.findElement(By.id("user-name"));
         userBox.sendKeys("standarduser");
         Thread.sleep(2000);
 
-        //Paso 3
-        //Escribir el password secret_sauce
+        // Paso 3
+        // Escribir el password secret_sauce
         WebElement passBox = driver.findElement(By.id("password"));
         passBox.sendKeys("secret_sauce");
         Thread.sleep(2000);
 
-        //Paso 4
+        // Paso 4
         // Pulsar en el botón de login
         WebElement clickButton = driver.findElement(By.id("login-button"));
         clickButton.click();
@@ -54,13 +54,8 @@ public class ValidarLoginIncorrecto {
         } else {
             System.out.println("El mensaje de error no se muestra.");
         }
-
-
         Thread.sleep(2000);
-
         driver.quit();
-
-
     }
 }
 
