0b75034db7f1160fc9e7a93f0b91fa5731b980a1
Silvia Casado Banqueri <scasado@hiberus.com>
2022-05-02 15:17:40 +0200

    actualizacion ejercicio

--- src/main/java/com/hiberus/university/selenium/Inventario/EliminarProductoCarrito.java
+++ src/main/java/com/hiberus/university/selenium/Inventario/EliminarProductoCarrito.java
@@ -1,2 +1,38 @@
-package com.hiberus.university.selenium.Inventario;public class EliminarProductoCarrito {
+package com.hiberus.university.selenium.Inventario;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+public class EliminarProductoCarrito {
+    public static WebDriver driver;
+    public static void main(String[] args) {
+
+        //Paso0
+        String userProfile = "C:\\Users\\scasado\\AppData\\Local\\Google\\Chrome\\User Data\\Default";
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("user-data-dir=" + userProfile);
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        //Paso1 Ir a la pagina
+        driver.get("https://www.saucedemo.com/");
+        //Paso2 Escribir el username
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        //Paso3 Escribir el password
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        //Paso 4. Pulsar en el botón del Login
+        driver.findElement(By.id("login-button")).click();
+        //Paso5 Agregamos producto al carrito
+        driver.findElement(By.id("add-to-cart-Sauce-Labs-Onesie")).click();
+        //Paso 6 Eliminar producto carrito
+        driver.findElement(By.xpath("//button[@class='remove-sauce-labs-onesie']")).click();
+        driver.close();
+    }
 }
