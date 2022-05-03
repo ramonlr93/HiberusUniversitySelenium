bb5550d463ed2dcdefa425c6d624d93825495737
edurnerojas <edurnerojas>
2022-05-03 12:24:44 +0200

    Constantes creadas

---
+++ src/main/java/com/hiberus/university/selenium/Inventario/Constantes.java
@@ -0,0 +1,6 @@
+package com.hiberus.university.selenium.Inventario;
+
+public class Constantes {
+
+    public static String pathChrome ="C:\\Users\\pue\\AppData\\Local\\Google\\Chrome\\User Data\\Default";
+}
