0fb63d741170ff0330231f550eae953c2b7ba77a
Jonatan Villen Martinez <jvillen@hiberus.com>
2022-05-03 14:40:11 +0200

    use list in pageFactory

--- src/main/java/com/hiberus/university/selenium/pages/PagesFactory.java
+++ src/main/java/com/hiberus/university/selenium/pages/PagesFactory.java
@@ -1,67 +1,55 @@
 package com.hiberus.university.selenium.pages;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class PagesFactory
-{
-    private static ThreadLocal<PagesFactory> pagesFactories = new ThreadLocal<PagesFactory>();
-    private WebDriver driver;
+public class PagesFactory {
+  private static PagesFactory pagesFactories;
+  private WebDriver driver;
 
-    private CheckOutStepOnePage checkOutStepOnePage;
-    private CheckOutStepTwoPage checkOutStepTwoPage;
-    private InventoryPage inventoryPage;
-    private LoginPage loginPage;
-    private CartPage cartPage;
+  private CheckOutStepOnePage checkOutStepOnePage;
+  private CheckOutStepTwoPage checkOutStepTwoPage;
+  private InventoryPage inventoryPage;
+  private LoginPage loginPage;
+  private CartPage cartPage;
 
-    public static void start(WebDriver driver)
-    {
-        PagesFactory instance = new PagesFactory(driver);
-        pagesFactories.set(instance);
-    }
+  public static void start(WebDriver driver) {
+    pagesFactories = new PagesFactory(driver);
+  }
 
-    public static PagesFactory getInstance()
-    {
-        return pagesFactories.get();
-    }
+  public static PagesFactory getInstance() {
+    return pagesFactories;
+  }
 
-    private PagesFactory(WebDriver driver)
-    {
-        this.driver = driver;
-        checkOutStepOnePage = new CheckOutStepOnePage(driver);
-        checkOutStepTwoPage = new CheckOutStepTwoPage(driver);
-        inventoryPage = new InventoryPage(driver);
-        loginPage = new LoginPage(driver);
-        cartPage = new CartPage(driver);
-    }
+  private PagesFactory(WebDriver driver) {
+    this.driver = driver;
+    checkOutStepOnePage = new CheckOutStepOnePage(driver);
+    checkOutStepTwoPage = new CheckOutStepTwoPage(driver);
+    inventoryPage = new InventoryPage(driver);
+    loginPage = new LoginPage(driver);
+    cartPage = new CartPage(driver);
+  }
 
-    public WebDriver getDriver()
-    {
-        return driver;
-    }
+  public WebDriver getDriver() {
+    return driver;
+  }
 
-    public CheckOutStepOnePage getCheckOutStepOnePage()
-    {
-        return checkOutStepOnePage;
-    }
+  public CheckOutStepOnePage getCheckOutStepOnePage() {
+    return checkOutStepOnePage;
+  }
 
-    public CheckOutStepTwoPage getCheckOutStepTwoPage()
-    {
-        return checkOutStepTwoPage;
-    }
+  public CheckOutStepTwoPage getCheckOutStepTwoPage() {
+    return checkOutStepTwoPage;
+  }
 
-    public InventoryPage getInventoryPage()
-    {
-        return inventoryPage;
-    }
+  public InventoryPage getInventoryPage() {
+    return inventoryPage;
+  }
 
-    public LoginPage getLoginPage()
-    {
-        return loginPage;
-    }
+  public LoginPage getLoginPage() {
+    return loginPage;
+  }
 
-    public CartPage getCartPage()
-    {
-        return cartPage;
-    }
+  public CartPage getCartPage() {
+    return cartPage;
+  }
 }
