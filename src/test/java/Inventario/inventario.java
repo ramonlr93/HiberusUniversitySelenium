b84d6d964a8218223bf9ccb6233150d687b1dd83
marioEzquerro <marioezquerroruiz@gmail.com>
2022-05-05 17:19:07 +0200

    Añadido al carrito bien

--- src/test/java/Inventario/inventario.java
+++ src/test/java/Inventario/inventario.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -64,9 +65,39 @@ public class inventario
         driver.findElement(By.id("login-button")).click();
 
         //Validacion
-        boolean SauceLabsBoltTShirtVisible = driver.findElement(By.xpath("//div[contains(text(), 'Sauce Labs Bolt T-Shirt')]")).isDisplayed();
+        boolean SauceLabsBoltTShirtVisible;
+
+        try {
+            SauceLabsBoltTShirtVisible= driver.findElement(By.xpath("//div[contains(text(), 'Sauce Labs Bolt T-Shirt')]")).isDisplayed();
+        } catch (NoSuchElementException e) {
+            SauceLabsBoltTShirtVisible = false;
+        }
 
         Assert.assertTrue("PRUEBA FALLIDA - El elemento error no aparece", SauceLabsBoltTShirtVisible);
+    }
+
+    @Test
+    public void SauceLabsBoltTShirtSeAñadeACarro() throws  InterruptedException {
+        driver.get(ruta);
+//        pausa = new WebDriverWait(driver, 10, 1000);
+
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        //Validacion
+        driver.findElement(By.xpath("//div[contains(text(), 'Sauce Labs Bolt T-Shirt')]")).click();
+        boolean seAñadioAlCarrito;
+
+        try {
+            seAñadioAlCarrito = driver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).isDisplayed();
+        } catch (NoSuchElementException e) {
+            seAñadioAlCarrito = false;
+        }
+
+
+
+        Assert.assertTrue("PRUEBA FALLIDA - El item no se añadio", seAñadioAlCarrito);
     }
 
     @After
