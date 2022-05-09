c0cdd2e8d30a5e165523ae8a46197cb74feaedac
miguelcasado <mcasado197@gmail.com>
2022-05-09 15:25:31 +0200

    Subida de ejercicios

--- src/test/java/com/hiberus/university/selenium/checkout/CheckoutTest.java
+++ src/test/java/com/hiberus/university/selenium/checkout/CheckoutTest.java
@@ -62,8 +62,6 @@ public class CheckoutTest {
 
         driver.findElement(By.id("add-to-cart-sauce-labs-onesie")).click();
 
-        WebElement carrito = driver.findElement(By.xpath("//a[@class='shopping_cart_link']"));
-
         driver.findElement(By.id("shopping_cart_container")).click();
 
         driver.findElement(By.id("checkout")).click();
@@ -102,6 +100,53 @@ public class CheckoutTest {
         System.out.println(total);
 
         Assert.assertEquals("No coinciden los totales", total, sumaPrecios);
+
+        //Reiniciamos el carrito y cerramos sesión (para poder volver a pasar el test sin problema)
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        driver.findElement(By.id("reset_sidebar_link")).click();
+        driver.findElement(By.id("logout_sidebar_link")).click();
+
+    }
+
+    @Test
+    public void RealizarPedidoTest() {
+
+        driver.get("https://www.saucedemo.com");
+        //Thread.sleep(2000);
+
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        //Thread.sleep(2000);
+
+
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        //Thread.sleep(2000);
+
+
+        driver.findElement(By.id("login-button")).click();
+        //Thread.sleep(2000);
+
+        driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt")).click();
+
+        driver.findElement(By.id("add-to-cart-sauce-labs-fleece-jacket")).click();
+
+        driver.findElement(By.id("add-to-cart-sauce-labs-onesie")).click();
+
+        driver.findElement(By.id("shopping_cart_container")).click();
+
+        driver.findElement(By.id("checkout")).click();
+
+        driver.findElement(By.id("first-name")).sendKeys("Miguel");
+
+        driver.findElement(By.id("last-name")).sendKeys("Casado");
+
+        driver.findElement(By.id("postal-code")).sendKeys("47001");
+
+        driver.findElement(By.id("continue")).click();
+
+        driver.findElement(By.id("finish")).click();
+
+        Assert.assertEquals("Fallo al realizar el pedido", "Your order has been dispatched, and will arrive just as fast as the pony can get there!", driver.findElement(By.xpath("//div[@class='complete-text']")).getText());
 
         //Reiniciamos el carrito y cerramos sesión (para poder volver a pasar el test sin problema)
         driver.findElement(By.id("react-burger-menu-btn")).click();
---
+++ src/test/java/com/hiberus/university/selenium/logout/LogoutTest.java
@@ -0,0 +1,65 @@
+package com.hiberus.university.selenium.logout;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class LogoutTest {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+
+    @Before
+    public void setUp() {
+        // Paso 0
+        String userProfile = "C:\\Users\\migue\\AppData\\Local\\Google\\Chrome\\User Data\\Default";
+        WebDriverManager.chromedriver().setup(); //cargar Chromedriver
+        ChromeOptions options = new ChromeOptions(); //crear instancia para opciones de chrome
+        options.addArguments("user-data-dir=" + userProfile);
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10, 500);
+
+    }
+
+    @Test
+    public void ComprobarLogoutTest() {
+
+        driver.get("https://www.saucedemo.com");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button")).click();
+
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+
+        driver.findElement(By.id("logout_sidebar_link")).click();
+
+        Assert.assertEquals("Fallo en el logout", "https://www.saucedemo.com/", driver.getCurrentUrl());
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
