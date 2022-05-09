e94fd8d2e13d004184110b62a67d7576ed1eead9
marioEzquerro <marioezquerroruiz@gmail.com>
2022-05-09 09:37:51 +0200

    📝 Refactorizado con constantes

---
+++ src/test/java/Common/constants.java
@@ -0,0 +1,7 @@
+package Common;
+
+public class constants {
+    public static final String ulrPruebas = "https://saucedemo.com/";
+    public static final String userProfile = "C:\\Users\\mezquerro\\AppData\\Local\\Google\\Chrome\\User Data\\Default";
+
+}
