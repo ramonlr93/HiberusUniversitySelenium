718aae0c8ca08ee2c6f5c07f910a8bb3c3ca3396
edurnerojas <edurnerojas>
2022-05-10 18:42:09 +0200

    Ejercicios realizados hasta la fecha

--- src/main/java/com/hiberus/university/selenium/login/RealizarLogin.java
+++ src/main/java/com/hiberus/university/selenium/login/RealizarLogin.java
@@ -1,11 +1,13 @@
 package com.hiberus.university.selenium.Login;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
+import com.hiberus.university.selenium.Inventario.Constantes;
 
 public class RealizarLogin {
 
@@ -21,6 +23,36 @@ public class RealizarLogin {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         // Paso1
+
+        // Paso 1
+
+        driver.get("https://www.saucedemo.com/");
+
+        //Paso 2
+        //Escribir el username
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        Thread.sleep(2000);
+
+        //Paso 3
+        //Escribir el password
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        Thread.sleep(2000);
+
+        //Paso4
+        //Pulsar LOGIN
+        driver.findElement(By.id("login-button")).click();
+        Thread.sleep(2000);
+
+        //Paso 5
+        //Validar la URL https://www.saucedemo.com/inventory.html
+        //driver.getCurrentUrl(); // devuelve la URL en la que estás
+        if (driver.getCurrentUrl().equals("https://www.saucedemo.com/inventory.html")) {
+            System.out.println("Url correcta");
+        } else {
+            System.out.println("Url incorrecta");
+
+        }
+
         driver.close();
     }
 }
