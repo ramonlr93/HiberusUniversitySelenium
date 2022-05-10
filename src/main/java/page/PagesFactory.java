f0de8e97289192122fac5dcc0bbb0a3e811fa75c
marioEzquerro <marioezquerroruiz@gmail.com>
2022-05-10 20:02:12 +0200

    Preparado

---
+++ src/main/java/page/PagesFactory.java
@@ -0,0 +1,24 @@
+package page;
+import org.openqa.selenium.WebDriver;
+
+public class PagesFactory {
+
+    private static PagesFactory pagesFactories;
+    private final WebDriver driver;
+
+    public PagesFactory(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public static void start(WebDriver driver) {
+        pagesFactories = new PagesFactory(driver);
+    }
+
+    public static PagesFactory getInstance() {
+        return pagesFactories;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
