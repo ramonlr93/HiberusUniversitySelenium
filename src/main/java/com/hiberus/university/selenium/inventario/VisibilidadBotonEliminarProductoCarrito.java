d13c818f59f4111dbe0484f9b2fd069ba5b1ea80
marioEzquerro <marioezquerroruiz@gmail.com>
2022-05-10 19:14:16 +0200

    Añadida nueva rama

---
+++ src/main/java/com/hiberus/university/selenium/inventario/VisibilidadBotonEliminarProductoCarrito.java
@@ -0,0 +1,8 @@
+package com.hiberus.university.selenium.inventario;
+
+public class VisibilidadBotonEliminarProductoCarrito {
+
+    public static void main( String[] args ) throws InterruptedException {
+
+    }
+}
