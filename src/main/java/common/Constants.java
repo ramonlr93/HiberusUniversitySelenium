1b15c2508399ca6bdb9b1a99f4ab4ca2c2b5005b
marioEzquerro <marioezquerroruiz@gmail.com>
2022-05-10 18:18:23 +0200

    Soporte para ubuntu

---
+++ src/main/java/common/Constants.java
@@ -0,0 +1,7 @@
+package common;
+
+public class Constants {
+    public static final String ulrPruebas = "https://saucedemo.com/";
+    public static final String userProfile = "\\home\\mario\\.config\\google-chrome\\Default\\Download Service";
+
+}
