584fdbcbc9ea8edfddfcd1becfbc2a6157c414a8
moniLamas <mlamasrial@gmail.com>
2022-05-11 15:56:26 +0200

    create page object model

---
+++ src/main/java/com/hiberus/university/selenium/login/ValidarLoginCorrecto.java
@@ -0,0 +1,11 @@
+package com.hiberus.university.selenium.login;
+
+
+import org.junit.Assert;
+
+public class ValidarLoginCorrecto {
+    public static void main(String[] args) {
+        Assert.assertEquals(true, true);
+    }
+
+}
---
+++ src/main/java/com/hiberus/university/selenium/pages/PagesFactory.java
@@ -0,0 +1,24 @@
+package com.hiberus.university.selenium.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class PagesFactory {
+    public static PagesFactory pagesFactories;
+    private final WebDriver driver;
+
+    public PagesFactory(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public static void start(WebDriver driver) {
+        pagesFactories = new PagesFactory(driver);
+    }
+
+    public static PagesFactory getInstance() {
+        return pagesFactories;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
