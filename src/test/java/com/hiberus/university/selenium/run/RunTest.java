c314c6309c07961397897645a4954f0582c208a1
moniLamas <mlamasrial@gmail.com>
2022-05-11 14:29:07 +0200

    add Ruben's solution Run

--- src/test/java/com/hiberus/university/selenium/run/RunTest.java
+++ src/test/java/com/hiberus/university/selenium/run/RunTest.java
@@ -1,7 +1,11 @@
 package com.hiberus.university.selenium.run;
 
+import com.hiberus.university.selenium.carrito.CarTSuiteTest;
+import com.hiberus.university.selenium.checkout.CheckoutSuiteTest;
+import com.hiberus.university.selenium.inventario.InventarioSuiteTest;
 import com.hiberus.university.selenium.login.LoginSuiteTest;
 
+import com.hiberus.university.selenium.logout.LogOutSuiteTest;
 import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 
@@ -9,9 +13,13 @@ import org.junit.runner.RunWith;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         LoginSuiteTest.class,
+        LogOutSuiteTest.class,
         InventarioSuiteTest.class,
+        CheckoutSuiteTest.class,
+        CarTSuiteTest.class
 })
 
 public class RunTest {
 
 }
+
