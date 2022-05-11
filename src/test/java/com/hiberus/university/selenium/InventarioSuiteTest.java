625676823c06dbf139e12eb43b3c10e10770f201
moniLamas <mlamasrial@gmail.com>
2022-05-11 14:46:02 +0200

    create page object model

--- src/test/java/com/hiberus/university/selenium/InventarioSuiteTest.java
+++ src/test/java/com/hiberus/university/selenium/InventarioSuiteTest.java
@@ -1,4 +1,4 @@
-package com.hiberus.university.selenium.inventario;
+package com.hiberus.university.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
--- src/test/java/com/hiberus/university/selenium/run/RunTest.java
+++ src/test/java/com/hiberus/university/selenium/run/RunTest.java
@@ -2,7 +2,7 @@ package com.hiberus.university.selenium.run;
 
 import com.hiberus.university.selenium.carrito.CarTSuiteTest;
 import com.hiberus.university.selenium.checkout.CheckoutSuiteTest;
-import com.hiberus.university.selenium.inventario.InventarioSuiteTest;
+import com.hiberus.university.selenium.InventarioSuiteTest;
 import com.hiberus.university.selenium.login.LoginSuiteTest;
 
 import com.hiberus.university.selenium.logout.LogOutSuiteTest;
