a2b95d4e5b7b27f2b315c7035127224035bb989e
monipeny <mlamasrial@gmail.com>
2022-05-12 15:37:04 +0200

    apply change LoginSuiteTest

--- src/main/java/com/hiberus/university/selenium/login/RealizarLogin.java
+++
@@ -1,9 +0,0 @@
-package com.hiberus.university.selenium.login;
-
-import org.junit.Assert;
-public class RealizarLogin {
-    public static void main( String[] args ) {
-          Assert.assertEquals( true, true);
-
-    }
-}
--- src/test/java/com/hiberus/university/selenium/run/RunTest.java
+++ src/test/java/com/hiberus/university/selenium/run/RunTest.java
@@ -1,10 +1,10 @@
 package com.hiberus.university.selenium.run;
 
-import com.hiberus.university.selenium.carrito.CarTSuiteTest;
+import com.hiberus.university.selenium.carrito.CartSuiteTest;
 import com.hiberus.university.selenium.checkout.CheckoutSuiteTest;
 import com.hiberus.university.selenium.InventarioSuiteTest;
-import com.hiberus.university.selenium.login.LoginSuiteTest;
 
+import com.hiberus.university.selenium.login.LoginSuiteTest;
 import com.hiberus.university.selenium.logout.LogOutSuiteTest;
 import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         LogOutSuiteTest.class,
         InventarioSuiteTest.class,
         CheckoutSuiteTest.class,
-        CarTSuiteTest.class
+        CartSuiteTest.class
 })
 
 public class RunTest {
