89bf7a4dffa78893c68c89cc467c381d72ed5973
miguelcasado <mcasado197@gmail.com>
2022-05-12 17:37:24 +0200

    Ejercicios

--- src/test/java/com/hiberus/university/selenium/login/LoginSuite.java
+++
@@ -1,4 +0,0 @@
-package com.hiberus.university.selenium.login;
-
-public class LoginSuite {
-}
--- src/test/java/com/hiberus/university/selenium/logout/LogoutTest.java
+++ src/test/java/com/hiberus/university/selenium/logout/LogoutTest.java
@@ -7,59 +7,59 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class LogoutTest {
 
     public static WebDriver driver;
-    public static WebDriverWait wait;
 
+    public static WebDriverWait wait;
 
     @Before
     public void setUp() {
-        // Paso 0
-        String userProfile = "C:\\Users\\migue\\AppData\\Local\\Google\\Chrome\\User Data\\Default";
-        WebDriverManager.chromedriver().setup(); //cargar Chromedriver
-        ChromeOptions options = new ChromeOptions(); //crear instancia para opciones de chrome
-        options.addArguments("user-data-dir=" + userProfile);
+        //Paso0
+        WebDriverManager.chromedriver().setup(); // Cargar Chromedriver
 
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver= new ChromeDriver();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, 10, 500);
 
+        wait = new WebDriverWait(driver, 10, 500);
     }
 
     @Test
-    public void ComprobarLogoutTest() {
+    public void logoutValidationTest() {
+        // Ir a la página https://www.saucedemo.com
+        driver.get("https://www.saucedemo.com/");
 
-        driver.get("https://www.saucedemo.com");
-
+        // Escribir el username standard_user
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
 
+        // Escribir el password secret_sauce
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
 
+        // Pulsar en el botón del Login
         driver.findElement(By.id("login-button")).click();
 
+        // Realizar el Logout
         driver.findElement(By.id("react-burger-menu-btn")).click();
-
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//nav[@class='bm-item-list']"))));
         driver.findElement(By.id("logout_sidebar_link")).click();
 
-        Assert.assertEquals("Fallo en el logout", "https://www.saucedemo.com/", driver.getCurrentUrl());
+        // Validar que el logout se ha realizado llevándonos a la página del Login
+        String loginPageUrl = driver.getCurrentUrl();
+        Assert.assertEquals("NO SE HA REALIZADO EL LOGOUT, NO ESTAMOS EN LA PAGINA DEL LOGIN ",
+                "https://www.saucedemo.com/", loginPageUrl);
 
     }
 
     @After
     public void tearDown() {
-        driver.quit();
+        driver.close();
     }
-
 }
