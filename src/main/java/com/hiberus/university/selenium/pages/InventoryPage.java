cb53db01953f6de7081548d576542bbad2be3ceb
monipeny <mlamasrial@gmail.com>
2022-05-12 18:15:32 +0200

    fix myfluentwait

--- src/main/java/com/hiberus/university/selenium/pages/InventoryPage.java
+++ src/main/java/com/hiberus/university/selenium/pages/InventoryPage.java
@@ -7,51 +7,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
-/* MI CÓDIGO
-@Slf4j
-public class InventoryPage extends AbstractPage {
-    public static final String PAGE_URL = "https://www.saucedemo.com";
-
-    @FindBy(xpath = "//div[@class='inventory_item_description']")
-    private List<WebElement> inventoryItems;
-
-    @FindBy(xpath = "//select[@class='product_sort_container']")
-    private WebElement sortProductBtn;
-
-
-//    @FindBy(id = "react-burger-menu-btn")
-//    private WebElement menuBtn;
-
-    InventoryPage(WebDriver driver) {
-        super(driver);
-    }
-
-    @Override
-    public WebElement getPageLoadedTestElement() {
-        return null;
-    }
-
-    public void clickAddToCartButton(String itemName) {
-        for (WebElement elm : inventoryItems) {
-            elm.findElement(By.xpath("//button[contains(@id='add-"+itemName+"')]")).click();
-        }
-    }
-
-    public void clickRemoveCartButton(String itemName) {
-        for (WebElement elm : inventoryItems) {
-            elm.findElement(By.xpath("//button[contains(@id='remove-"+itemName+"')]")).click();
-        }
-    }
-
-    public void sortItemsBy(String order) {
-        sortProductBtn.click();
-        sortProductBtn.findElement(By.xpath("//option[@value='"+order+"']")).click();
-    }
-
-} */
-
-// SOLUCIÓN JONATAN
-
 public class InventoryPage extends AbstractPage {
   public static final String PAGE_URL = "https://www.saucedemo.com/inventory.html";
 
--- src/main/java/com/hiberus/university/selenium/pages/PagesFactory.java
+++ src/main/java/com/hiberus/university/selenium/pages/PagesFactory.java
@@ -2,31 +2,6 @@ package com.hiberus.university.selenium.pages;
 
 import org.openqa.selenium.WebDriver;
 
-
-/* MI CÓDIGO
-public class PagesFactory {
-
-    private static PagesFactory pagesFactories;
-    private final WebDriver driver;
-
-    public PagesFactory(WebDriver driver) {
-        this.driver = driver;
-    }
-
-    public static void start(WebDriver driver) {
-        pagesFactories = new PagesFactory(driver);
-    }
-
-    public static PagesFactory getInstance() {
-        return pagesFactories;
-    }
-
-    public WebDriver getDriver() {
-        return driver;
-    }
-} */
-
-// SOLUCIÓN JONATAN
 public class PagesFactory {
 
     private static PagesFactory pagesFactories;
