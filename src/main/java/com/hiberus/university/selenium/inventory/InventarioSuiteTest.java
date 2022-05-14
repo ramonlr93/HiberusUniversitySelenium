a683659e15fee0401a48c43ffd66ab9b21c4aa04
monipeny <mlamasrial@gmail.com>
2022-05-14 13:08:52 +0200

    apply change InventorySuiteTest

--- src/main/java/com/hiberus/university/selenium/inventory/InventarioSuiteTest.java
+++ src/main/java/com/hiberus/university/selenium/inventory/InventarioSuiteTest.java
@@ -1,4 +1,4 @@
-package com.hiberus.university.selenium.inventario;
+package com.hiberus.university.selenium.inventory;
 
 import com.hiberus.university.selenium.pages.CartPage;
 import com.hiberus.university.selenium.pages.InventoryPage;
