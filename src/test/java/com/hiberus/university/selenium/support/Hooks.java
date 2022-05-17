a758b2ccce8fec7bdc0371b3abf3f1229ef73173
monipeny <mlamasrial@gmail.com>
2022-05-17 20:05:54 +0200

    create Hooks.java

--- src/test/java/com/hiberus/university/selenium/support/Hooks.java
+++ src/test/java/com/hiberus/university/selenium/support/Hooks.java
@@ -5,10 +5,10 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Hooks {
@@ -16,21 +16,20 @@ public class Hooks {
     public static WebDriver driver;
 
     @Before()
-    public static void before(Scenario scenario) {
-
+    public static void before(Scenario scenario){
+        log.info("starting test" + scenario.getName());
         WebDriverManager.chromedriver().setup();
-
         driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
         PagesFactory.start(driver);
-
     }
 
     @After()
-    public static void after(Scenario scenario) {
+    public static void after(Scenario scenario){
+        log.info("ending test" + scenario.getName());
         driver.close();
     }
 }
