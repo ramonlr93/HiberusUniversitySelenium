e70a58c38afde5aa2cf5b90911fc5cf6c2a839cb
Santos-P-R <santospenaroldan3@gmail.com>
2022-05-31 19:45:07 +0200

    Ejercicio 8

--- src/test/java/com/hiberus/university/selenium/support/Hooks.java
+++ src/test/java/com/hiberus/university/selenium/support/Hooks.java
@@ -1,35 +1,74 @@
 package com.hiberus.university.selenium.support;
 
 import com.hiberus.university.selenium.pages.PagesFactory;
+import com.hiberus.university.selenium.utils.Flags;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import java.util.concurrent.TimeUnit;
+
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.jsoup.internal.StringUtil;
+import org.testng.annotations.Parameters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import static org.openqa.selenium.remote.BrowserType.*;
+
 
 @Slf4j
 public class Hooks {
 
-  public static WebDriver driver;
+    public static WebDriver driver;
 
-  @Before()
-  public static void before(Scenario scenario){
-    log.info("starting test" + scenario.getName());
-    WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver();
-    driver.manage().deleteAllCookies();
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    driver.manage().window().maximize();
+    @Before()
+    public static void before(Scenario scenario) {
+        log.info("starting test" + scenario.getName());
+        String browser = Flags.getInstance().getBrowser();
+        Boolean isHeadLess = Flags.getInstance().isHeadless();
+        if (StringUtils.isBlank(browser)){
+            browser = CHROME;
+        }
+        switch (browser){
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions optionsF = new FirefoxOptions();
+                if(isHeadLess) {
+                    optionsF.setHeadless(isHeadLess);
+                }
+                driver = new FirefoxDriver(optionsF);
+                break;
+            case EDGE:
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions optionsC = new ChromeOptions();
+                if(isHeadLess) {
+                    optionsC.setHeadless(isHeadLess);
+                }
+                driver = new ChromeDriver(optionsC);
+                break;
+        }
 
-    PagesFactory.start(driver);
-  }
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        PagesFactory.start(driver);
+    }
 
-  @After()
-  public static void after(Scenario scenario){
-    log.info("ending test" + scenario.getName());
-    driver.close();
-  }
+
+    @After()
+    public static void after(Scenario scenario) {
+        log.info("ending test" + scenario.getName());
+        driver.close();
+    }
 }
--- src/test/resources/login.feature
+++ src/test/resources/login.feature
@@ -3,23 +3,25 @@ Feature: Login test suite
 
   Background:
     Given the user is on the home page
+
   @smoke
+    @testcase01
   Scenario Outline: Verify valid user can login
     And the user provides the username "<username>"
     And the user provides the password "<password>"
     When the user clicks the login button
     Then the user is logged successfully
-
     Examples:
-      | username        | password      |
-      | standard_user   | secret_sauce  |
+      | username      | password     |
+      | standard_user | secret_sauce |
+
 
   Scenario Outline: Verify valid user cannot login
     And the user provides the username "<username>"
     And the user provides the password "<password>"
     When the user clicks the login button
     Then the user should be shown and invalid message
-
+    @testcase02
     Examples:
-      | username   | password      |
-      | bad_user   | secret_sauce  |
+      | username | password     |
+      | bad_user | secret_sauce |
